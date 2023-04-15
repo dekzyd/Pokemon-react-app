31bad495edbfdcfeba38602a1d3f7afb8a89aad6
dekzyd <adekoyadayo2010@gmail.com>
2023-04-15 10:05:23 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
 
-function App() {
+//Pages
+import Homepage from './pages/Homepage';
+import PokemonPage from './pages/PokemonPage';
+import Header from './components/Header';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Container>
+      <Header/>
+        <Routes>
+          <Route exact path='/' Component={Homepage} />
+          <Route path='/pokemon/:id' Component={PokemonPage} />
+        </Routes>
+      </Container>
+    </Router>
   );
 }
 
